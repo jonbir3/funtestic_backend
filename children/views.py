90bd4187db6b1c8afbf48c2ab17aa9ba1fe779d5
shlomi <shlomitofahi@gmail.com>
2019-05-21 18:49:48 +0300

    add child list get to view

--- children/views.py
+++ children/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from children.serializers import ChildrenSerializer
+from . models import Child
+
+
+class childList(APIView):
+
+    def get(self, request):
+        child = Child.objects.all()
+        serializer = ChildrenSerializer(child, many=True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
