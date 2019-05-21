ee965e506b618438d7e5c9c2d96517121a00d716
Yoni <jonbir3@gmail.com>
2019-05-21 17:51:34 +0300

    create all the models

--- children/models.py
+++ children/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import Person
 
-# Create your models here.
+
+class Child(models.Model):
+    GENDER = (('M', 'Male'),
+              ('F', 'Female'),
+              ('O', 'Other'))
+    age = models.CharField(max_length=2)
+    gender = models.CharField(choices=GENDER, default='M', max_length=1)
+    name = models.CharField(max_length=20)
+    parent = models.ForeignKey(Person, on_delete=models.CASCADE)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+MIN_LENGTH_OF_ID = 8
+MAX_LENGTH_OF_ID = 9
+
+
+class Person(models.Model):
+    id_number = models.CharField(max_length=MAX_LENGTH_OF_ID,
+                                 validators=[RegexValidator(regex='^[0-9]*$', message='Numbers only', code='num_only')])
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
