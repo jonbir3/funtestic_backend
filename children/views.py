ad493d145482efda11c53d058e1804c1d332e39b
shlomi <shlomitofahi@gmail.com>
2019-05-22 18:26:39 +0300

    add change from get to post respond, finished post method

--- children/views.py
+++ children/views.py
@@ -3,15 +3,26 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from children.serializers import ChildrenSerializer
+from users.models import Person
 from . models import Child
 
 
-class childList(APIView):
+class ChildList(APIView):
 
-    def get(self, request):
-        child = Child.objects.all()
-        serializer = ChildrenSerializer(child, many=True)
+    def post(self, request):
+        parent_id = request.data['id']
+        parent = Person.objects.get(id_number=parent_id)
+
+        children_of_parent = Child.objects.filter(parent=parent)
+
+        serializer = ChildrenSerializer(children_of_parent, many=True)
         return Response(serializer.data)
 
-    def post(self):
-        pass
+    def put(self, request):
+        parent_id = request.data['parent_id']
+        parent = Person.objects.get(id_number=parent_id)
+
+        child_id = request.data['child_id']
+        child_age = request.data['child_age']
+        child_gander = request.data['child_gander']
+        child_name = request.data['child_name']
