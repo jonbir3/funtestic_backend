a4a84596a3ef7505a0702e2001bae9f4fe8ecdd0
shlomi <shlomitofahi@gmail.com>
2019-05-22 19:31:07 +0300

    add put method to  API view

--- children/views.py
+++ children/views.py
@@ -10,10 +10,14 @@ from . models import Child
 class ChildList(APIView):
 
     def post(self, request):
-        parent_id = request.data['id']
-        parent = Person.objects.get(id_number=parent_id)
-
-        children_of_parent = Child.objects.filter(parent=parent)
+        try:
+            parent_id = request.data['id']
+            parent = Person.objects.get(id_number=parent_id)
+            children_of_parent = Child.objects.filter(parent=parent)
+        except parent.DoesNotExist:
+            return Response({'error parent does not exist': self.code_404}, 404)
+        except children_of_parent.DoesNotExist :
+            return Response({'error child does not exist': self.code_404}, 404)
 
         serializer = ChildrenSerializer(children_of_parent, many=True)
         return Response(serializer.data)
@@ -22,7 +26,9 @@ class ChildList(APIView):
         parent_id = request.data['parent_id']
         parent = Person.objects.get(id_number=parent_id)
 
-        child_id = request.data['child_id']
         child_age = request.data['child_age']
         child_gander = request.data['child_gander']
         child_name = request.data['child_name']
+
+        child = Child(parent=parent, name=child_name, gender=child_gander, age=child_age)
+        child.save()
