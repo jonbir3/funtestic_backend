511eb7d8d4cfb9a90df2358ae216b4f98b5355d4
shlomi <shlomitofahi@gmail.com>
2019-05-22 19:30:10 +0300

    add phone number to person

--- users/models.py
+++ users/models.py
@@ -4,11 +4,13 @@ from django.core.validators import RegexValidator
 
 MIN_LENGTH_OF_ID = 8
 MAX_LENGTH_OF_ID = 9
-
+LENGTH_OF_PHONE = 10
 
 class Person(models.Model):
     id_number = models.CharField(max_length=MAX_LENGTH_OF_ID,
                                  validators=[RegexValidator(regex='^[0-9]*$', message='Numbers only', code='num_only')])
+    phone_number = models.CharField(max_length=LENGTH_OF_PHONE,
+                                    validators=[RegexValidator(regex='^[0-9]*$', message='Numbers only', code='num_only')])
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
