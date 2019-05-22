dcdb6265fe037c552d095bb3439a9fcb0fd54e99
shlomi <shlomitofahi@gmail.com>
2019-05-23 00:17:34 +0300

    commit to yoni ha hofer!!!!

--- children/views.py
+++ children/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from children.serializers import ChildrenSerializer
 from users.models import Person
 from . models import Child
+from cryptography.utils import CbcEngine
+from funtestic_backend.settings import DES_KEY, CBC_IV
 
 
 class ChildList(APIView):
+    def get(self):
+        return Response({'Forbidden': self.code_404}, 404)
 
     def post(self, request):
         try:
@@ -26,9 +31,30 @@ class ChildList(APIView):
         parent_id = request.data['parent_id']
         parent = Person.objects.get(id_number=parent_id)
 
-        child_age = request.data['child_age']
-        child_gander = request.data['child_gander']
-        child_name = request.data['child_name']
+        cbc_engine = CbcEngine(DES_KEY, CBC_IV)
+
+        child_age = cbc_engine.encrypt(request.data['age'])
+        child_gander = cbc_engine.encrypt(request.data['gander'])
+        child_name = cbc_engine.encrypt(request.data['name'])
 
         child = Child(parent=parent, name=child_name, gender=child_gander, age=child_age)
-        child.save()
+
+        serializer = ChildrenSerializer(child, data=request.data)
+        print(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def put(self, request):
+    #     parent_id = request.data['parent_id']
+    #     parent = Person.objects.get(id_number=parent_id)
+    #
+    #     cbc_engine = CBC('wwwwwwww', 'qqqqqqqq')
+    #
+    #     child_age = cbc_engine.encrypt(request.data['child_age'])
+    #     child_gander = cbc_engine.encrypt(request.data['child_gander'])
+    #     child_name = cbc_engine.encrypt(request.data['child_name'])
+    #
+    #     child = Child(parent=parent, name=child_name, gender=child_gander, age=child_age)
+    #     child.save()
