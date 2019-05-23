f6e2153474ffcfa10be2b5731006f07152e3c593
Yoni <jonbir3@gmail.com>
2019-05-23 12:58:33 +0300

    add is number to child

--- children/models.py
+++ children/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from users.models import Person
+from django.core.validators import RegexValidator
+
+MAX_LENGTH_OF_ID = 9
 
 
 class Child(models.Model):
     GENDER = (('M', 'Male'),
               ('F', 'Female'),
               ('O', 'Other'))
+    id_number = models.CharField(max_length=MAX_LENGTH_OF_ID,
+                                 validators=[RegexValidator(regex='^[0-9]*$',
+                                                            message='Numbers only',
+                                                            code='num_only')],
+                                 primary_key=True)
     age = models.CharField(max_length=2)
     gender = models.CharField(choices=GENDER, default='M', max_length=1)
     name = models.CharField(max_length=20)
