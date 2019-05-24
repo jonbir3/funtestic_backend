ad77eb96b54b37e1743bd9b119e4768cf43a4c9d
Yoni <jonbir3@gmail.com>
2019-05-24 18:30:08 +0300

    add custom Person manager

--- users/models.py
+++ users/models.py
@@ -8,7 +8,46 @@ MAX_LENGTH_OF_ID = 9
 LENGTH_OF_PHONE = 10
 
 
+class PersonQuerySet(models.query.QuerySet):
+
+    def get(self, **kwargs):
+        for key, val in kwargs.items():
+            kwargs[key] = CbcEngine.get_engine().encrypt(val)
+        person = super(PersonQuerySet, self).get(**kwargs)
+
+        cbc_engine = CbcEngine.get_engine()
+        person.phone_number = cbc_engine.decrypt(person.phone_number)
+        user = person.user
+        user.username = cbc_engine.decrypt(user.username)
+        user.first_name = cbc_engine.decrypt(user.first_name)
+        user.last_name = cbc_engine.decrypt(user.last_name)
+        user.email = cbc_engine.decrypt(user.email)
+        person.user = user
+        return person
+
+    # def create(self, **kwargs):
+    #     for key, val in kwargs.items():
+    #         if key == 'user':
+    #             kwargs[key] = dict(val)
+    #             for user_key, user_val in kwargs[key].items():
+    #                 if user_key is not 'password':
+    #                     kwargs[key][user_key] = CbcEngine.get_engine().encrypt(user_val)
+    #         else:
+    #             kwargs[key] = CbcEngine.get_engine().encrypt(val)
+    #
+    #     user = User.objects.create(kwargs['user'])
+    #     kwargs['user'] = user
+    #
+    #     return super(PersonQuerySet, self).create(**kwargs)
+
+
+class PersonManager(models.Manager.from_queryset(PersonQuerySet)):
+    pass
+
+
 class Person(models.Model):
+    objects = PersonManager()
+
     phone_number = models.CharField(max_length=LENGTH_OF_PHONE,
                                     validators=[RegexValidator(regex='^[0-9]*$',
                                                                message='Numbers only',
