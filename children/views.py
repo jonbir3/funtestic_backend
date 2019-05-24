104151c15430364e54a2f2f9023151b055c478e6
Yoni <jonbir3@gmail.com>
2019-05-24 15:34:52 +0300

    fix get all children

--- children/views.py
+++ children/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from rest_framework import status
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from children.serializers import ChildrenSerializer
@@ -10,18 +10,20 @@ from cryptography.utils import CbcEngine
 
 
 class ChildList(APIView):
-    def get(self, request):
-        return Response('Forbidden', status=status.HTTP_400_BAD_REQUEST)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def post(self, request):
         try:
             parent_id = CbcEngine.get_engine().encrypt(request.data['id'])
-            parent = Person.objects.get(id_number=parent_id)
+            parent = Person.objects.get(phone_number=parent_id)
             children_of_parent = Child.objects.filter(parent=parent)
+        except KeyError:
+            return Response('id field is missing.', status=status.HTTP_400_BAD_REQUEST)
         except parent.DoesNotExist:
-            return Response({'error parent does not exist': self.code_404}, 404)
+            return Response('Parent does not exist.', status=status.HTTP_400_BAD_REQUEST)
         except children_of_parent.DoesNotExist:
-            return Response({'error child does not exist': self.code_404}, 404)
+            return Response('The parent has no children.', status=status.HTTP_400_BAD_REQUEST)
 
         serializer = ChildrenSerializer(children_of_parent, many=True)
         return Response(serializer.data)
