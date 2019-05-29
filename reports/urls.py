4aae6c97a9c0afb4fea0f189608ef82b4d61a896
shlomi <shlomitofahi@gmail.com>
2019-05-29 09:38:40 +0300

    change url from /add to /save

--- reports/urls.py
+++ reports/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from reports import views as reports_view
 
 urlpatterns = [
-    url(r'^add/', reports_view.ReportList.as_view(), name='get_all'),
+    url(r'^save/', reports_view.ReportList.as_view(), name='get_all'),
 
 ]
