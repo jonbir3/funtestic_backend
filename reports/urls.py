83f7d1f55e807bce68d722e46b1bca7658297123
May hagbi <mayhagbi@gmail.com>
2019-05-31 19:26:29 +0300

    add url for send report at email

--- reports/urls.py
+++ reports/urls.py
@@ -1,3 +1,6 @@
 from django.conf.urls import url
+from reports import views as reports_view
 
-urlpatterns = []
+urlpatterns = [
+    url(r'^sendReport/', reports_view.SendReportToEmail.as_view(), name='send_report'),
+]
